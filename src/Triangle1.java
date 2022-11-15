de65118bcfe49e3eb32c6852d3fb7a9beb196dd3
DAdarsh08 <112772798+DAdarsh08@users.noreply.github.com>
2022-11-15 07:23:12 -0800

    this reference variable and reference variables in use trial

---
+++ src/Triangle1.java
@@ -0,0 +1,29 @@
+public class Triangle1 {
+    int a,b,c;
+    public Triangle1(int a, int b, int c)
+    {
+        this.a=a;
+        this.b=b;
+        this.c=c;
+
+    }
+    public Triangle1 currentObject()
+    {
+        return this;
+    }
+    public String toString(){
+        return "Triangle{"+"a="+a+",b="+b+",c="+c+"}";
+    }
+
+    public static void main(String[] args) {
+        Triangle1 t1=new Triangle1(4,5,6);
+        System.out.println(t1);
+        Triangle1 x=t1.currentObject();
+        x.a=5;
+        System.out.println(t1);
+        System.out.println(x);
+        System.out.println(t1==x);
+
+    }
+
+}
---
+++ src/Trinangle.java
@@ -0,0 +1,24 @@
+public class Trinangle {
+    int a,b,c;
+    public Trinangle(int a,int b,int c)
+    {
+       this.a=a;
+       this. b=b;
+       this.c =c;
+        System.out.println(this);
+    }
+    public Trinangle get()
+    {
+        return this;
+    }
+    public String toString()
+    {
+        return "Triangle{"+"a="+a+",b="+b+",c="+c+"}";
+    }
+
+    public static void main(String[] args) {
+        Trinangle t1=new Trinangle(4,5,6);
+        System.out.println(t1);
+        System.out.println(t1==t1.get());
+    }
+}
