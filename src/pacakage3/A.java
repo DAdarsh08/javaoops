5712d0a53eeacb2640d9179bfb1fcc2f76b0cae1
DAdarsh08 <112772798+DAdarsh08@users.noreply.github.com>
2022-11-15 22:35:01 -0800

    package

---
+++ src/pacakage3/A.java
@@ -0,0 +1,12 @@
+package pacakage3;
+
+public class A {
+    public static void fstatic()
+    {
+        System.out.println(" static function in Class A in pacakage 3");
+    }
+    public void finstance()
+    {
+        System.out.println(" instance function in class A in package3");
+    }
+}
--- src/package2/B.java
+++ src/package2/B.java
@@ -4,8 +4,10 @@ package package2;
 //import pacakage1.A;//first method
 //import pacakage1.*;//second method
 //import pacakage1.A;// third method
-import pacakage1.A;//then there is a static import that allows static members to be used without a class name(4 method)
-import static pacakage1.A.fstatic;// we have to do this(4 method)
+//import pacakage1.A;//then there is a static import that allows static members to be used without a class name(4 method)
+//import static pacakage1.A.fstatic;// we have to do this(4 method)
+//import pacakage1.A;(5)if we change over to importing all classes a different error will be raise.the compiler cannot differentiate between the classes
+//import pacakage3.A;(5)
 
 
 
@@ -28,12 +30,24 @@ public class B {
       a.finstance();
       A.fstatic();
   }*/
-  public static void main(String[] args) {
+ /* public static void main(String[] args) {  i  used for both  4 and 5 methods)
       A a=new A();
       a.finstance();
+
+
       fstatic();//we used static members without the class names;
 
 
+  }*/
+  public static void main(String[] args) {//the onle code work for two same classes in different packages like in case of 5;
+      pacakage1.A a=new pacakage1.A();
+      a.finstance();
+      pacakage1.A.fstatic();
+      pacakage3.A b=new pacakage3.A();
+      b.finstance();
+      pacakage3.A.fstatic();
+
   }
+
 
 }
